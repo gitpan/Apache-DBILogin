f7def8d4810f5eeb944184901db94960af98d368
John D Groenveld <groenveld@acm.org>
2001-02-21 19:33:12 +0000

    Import of JGROENVEL/Apache-DBILogin-2.0 from CPAN.
    gitpan-cpan-distribution: Apache-DBILogin
    gitpan-cpan-version:      2.0
    gitpan-cpan-path:         JGROENVEL/Apache-DBILogin-2.0.tar.gz
    gitpan-cpan-author:       JGROENVEL
    gitpan-cpan-maturity:     released

--- DBILogin.pm
+++ DBILogin.pm
@@ -1,40 +1,39 @@
-# $Id: DBILogin.pm,v 1.6 2000/02/07 21:15:48 jdg117 Exp $
+# $Id: DBILogin.pm,v 1.2 2001/02/21 19:14:47 jdg117 Exp $
 package Apache::DBILogin;
 use strict;
 use Apache();
-use Apache::Constants qw(OK SERVER_ERROR AUTH_REQUIRED);
+use Apache::Constants qw(OK SERVER_ERROR AUTH_REQUIRED FORBIDDEN);
 use DBI;
 use vars qw($VERSION);
 
-$VERSION = '1.6';
+$VERSION = '2.0';
 my(%Config) = (
     'Auth_DBI_data_source' => '',
     'Auth_DBI_authz_command' => '',
+    'DBILogin_Oracle_authz_command' => '',
 );
 my $prefix = "Apache::DBILogin";
 
-sub handler {
-    my($r) = @_;
+sub authen {
+    my $r = shift @_;
+    return OK unless $r->is_initial_req;
+
     my($key,$val);
-    my $attr = {
-        DBType => 'SQL',
-    };
+    my $attr = {};
     while(($key,$val) = each %Config) {
         $val = $r->dir_config($key) || $val;
         $key =~ s/^Auth_DBI_//;
         $attr->{$key} = $val;
     }
     
-    return OK unless $r->is_initial_req;
-    return check($r, $attr);
+    return test_authen($r, $attr);
 }
  
-sub check {
+sub test_authen {
     my($r, $attr) = @_;
-    my($res, $sent_pwd);
  
-    ($res, $sent_pwd) = $r->get_basic_auth_pw;
-    return $res if $res; #decline if not Basic
+    my ($res, $sent_pwd) = $r->get_basic_auth_pw;
+    return $res if ( $res ); #decline if not Basic
 
     my $user = $r->connection->user;
 
@@ -43,13 +42,14 @@ sub check {
         return SERVER_ERROR;
     }
 
-    my $dbh = DBI->connect($attr->{data_source}, $user, $sent_pwd, { PrintError=>0, RaiseError=>0 });
+    my $dbh = DBI->connect($attr->{data_source}, $user, $sent_pwd, { AutoCommit=>0, RaiseError=>0 });
     unless( defined $dbh ) {
         $r->log_reason("user $user: $DBI::errstr", $r->uri);
         $r->note_basic_auth_failure;
         return AUTH_REQUIRED;
     }
 
+    # to be removed in next version
     if ( $attr->{authz_command} ) {
         unless( defined ($dbh->do($attr->{authz_command})) ) {
             $r->log_reason("user $user: $DBI::errstr", $r->uri);
@@ -59,56 +59,166 @@ sub check {
     }
            
     $dbh->disconnect;
-    $r->header_in('Modperl_Password',$sent_pwd); # deprecated
     $r->header_in('Modperl_DBILogin_Password',$sent_pwd);
     $r->header_in('Modperl_DBILogin_data_source',$attr->{data_source});
     return OK;
 }
+
+sub authz {
+    my $r = shift @_;
+    return OK unless $r->is_initial_req;
+
+    my $user = $r->connection->user;
+
+    my($key,$val);
+    my $attr = {};
+    while(($key,$val) = each %Config) {
+        $val = $r->dir_config($key) || $val;
+        $key =~ s/^Auth_DBI_//;
+        $attr->{$key} = $val;
+    }
+    
+    return test_authz($r, $attr);
+}
+
+sub test_authz {
+    my($r, $attr) = @_;
+
+    my ($res, $sent_pwd) = $r->get_basic_auth_pw;
+    return $res if ( $res ); #decline if not Basic
+
+    my $user = $r->connection->user;
+
+    unless ( $attr->{data_source} ) {
+        $r->log_reason("$prefix is missing the source parameter for database connect", $r->uri);
+        return SERVER_ERROR;
+    }
+
+    my $dbh = DBI->connect($attr->{data_source}, $user, $sent_pwd, {AutoCommit=>0, RaiseError=>0});
+    unless( defined $dbh ) {
+        $r->log_reason("user $user: $DBI::errstr", $r->uri);
+        return SERVER_ERROR;
+    }
+
+    my $authz_result = FORBIDDEN;
+    my $sth;
+    foreach my $requirement ( @{$r->requires} ) {
+        my $require = $requirement->{requirement};
+        if ( $require eq "valid-user" ) {
+            $authz_result = OK;
+        } elsif ( $require =~ s/^user\s+// ) { 
+                foreach my $valid_user (split /\s+/, $require) {
+                    if ( $user eq $valid_user ) {
+                        $authz_result = OK;
+                        last;
+                    }
+                }
+                if ( $authz_result != OK ) {
+                    my $explaination = <<END;
+<HTML>
+<HEAD><TITLE>Unauthorized</TITLE></HEAD>
+<BODY>
+<H1>Unauthorized</H1>
+User must be one of these required users: $require
+</BODY>
+</HTML>
+END
+                    $r->custom_response(FORBIDDEN, $explaination);
+                    $r->log_reason("user $user: not authorized", $r->uri);
+                }
+            } elsif ( $require =~ s/^group\s+// ) {
+                    foreach my $group (split /\s+/, $require) {
+                        $authz_result = is_member($r, $dbh, $group);
+                        last if ( $authz_result == OK );
+                        if ( $authz_result == SERVER_ERROR ) {
+                            $r->log_reason("user $user: $@", $r->uri);
+                            return SERVER_ERROR;
+                        }
+                    }
+                    if ( $authz_result == FORBIDDEN ) {
+                        my $explaination = <<END;
+<HTML>
+<HEAD><TITLE>Unauthorized</TITLE></HEAD>
+<BODY>
+<H1>Unauthorized</H1>
+User must be member of one of these required groups: $require
+</BODY>
+</HTML>
+END
+                        $r->custom_response(FORBIDDEN, $explaination);
+                        $r->log_reason("user $user: not authorized", $r->uri);
+                    }
+                }
+    }
+
+    $dbh->disconnect;
+    return $authz_result;
+}
+
 1;
  
 __END__
 
 =head1 NAME
 
-Apache::DBILogin - authenticates via a DBI connection
+Apache::DBILogin - authenticates and authorizes via a DBI connection
 
 =head1 SYNOPSIS
-
-See the access.conf file and the documentation for Apache::AuthenDBI
 
  #in .htaccess
  AuthName MyAuth
  AuthType Basic
- PerlHandler Apache::Registry::handler
- PerlAuthenHandler Apache::DBILogin::handler
- AddHandler perl-script pl
- 
+ PerlAuthenHandler Apache::DBILogin::authen
  PerlSetVar Auth_DBI_data_source dbi:Oracle:SQLNetAlias
- #PerlSetVar Auth_DBI_authz_command "SET ROLE DBA"
+ PerlAuthzHandler Apache::DBILogin::authz
  
- Options Indexes FollowSymLinks ExecCGI
- AllowOverride All
-  
  allow from all
- require valid-user
+ require group connect resource dba
  satisfy all
+
+ #in startup.pl
+ package Apache::DBILogin;
+ 
+ # is_member function for authz handler
+ #  expects a request object, database handle, and the group you which to test
+ #  returns a valid response code
+ sub is_member {
+     my ($r, $dbh, $group) = @_;
+ 
+     my $sth;
+     eval {
+         # no, Oracle doesn't support binding in SET ROLE statement
+         $sth = $dbh->prepare("SET ROLE $group") or die $DBI::errstr;
+     };
+     return SERVER_ERROR if ( $@ );
+        
+     return ( defined $sth->execute() ) ? OK : FORBIDDEN;
+ }
 
 =head1 DESCRIPTION
 
-Apache::DBILogin allows authentication against a multi-user database. It is
-intended to facilitate web-based transactions against a database server
-as a particular database user. If you wish authenticate against a passwd
-table instead, please see Edmund Mergl's Apache::AuthenDBI module.
+Apache::DBILogin allows authentication and authorization against a
+multi-user database.
 
-Auth_DBI_authz_command is an optional valid database command, executed via
-the DBI do method. In the above example I take advantage of Oracle roles, a
-set of priviledges which can be assigned to groups of users.
+It is intended to facilitate web-based transactions against a database server
+as a particular database user. If you wish authenticate against a passwd
+table instead, please see Edmund Mergl's Apache::AuthDBI module.
+
+Group authorization is handled by your Apache::DBILogin::is_member()
+function which you must define if you enable the authz handler.
+
+The above example uses Oracle roles to assign group membership. A role is a
+set of database privileges which can be assigned to users. Unfortunately,
+roles are vendor specific. Under Oracle you can test membership with
+"SET ROLE role_name" statement. You could also query the data dictionary,
+DBA_ROLE_PRIVS, but under Oracle that requires explicit privilege.
+Documentation patches for other databases are welcome.
 
 =head1 ENVIRONMENT
 
 Applications may access the clear text password as well as the data_source
 via the environment variables B<HTTP_MODPERL_DBILOGIN_PASSWORD> and
-B<HTTP_MODPERL_DBILOGIN_DATA_SOURCE>. B<HTTP_MODPERL_PASSWORD> is deprecated.
+B<HTTP_MODPERL_DBILOGIN_DATA_SOURCE>.
 
  #!/usr/bin/perl -wT
  
@@ -127,9 +237,18 @@ B<HTTP_MODPERL_DBILOGIN_DATA_SOURCE>. B<HTTP_MODPERL_PASSWORD> is deprecated.
 The database user's clear text passwd is made available in the
 server's environment. Do you trust your developers?
 
+=head1 BUGS
+
+Probably lots, I'm not the best programmer in the world.
+
+=head1 NOTES
+
+Feel free to email me with comments, suggestions, flames. Its the
+only way I'll become a better programmer.
+
 =head1 SEE ALSO
 
-mod_perl(1), Apache::DBI(3), Apache::AuthenDBI(3), and Apache::AuthzDBI(3)
+mod_perl(1), Apache::DBI(3), and Apache::AuthDBI(3)
 
 =head1 AUTHOR
 
